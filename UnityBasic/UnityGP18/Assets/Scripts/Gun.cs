516f86994b6a271561465ccb47e06239fd2fba56
downkhg <downkhg@gmail.com>
2021-04-05 21:50:04 +0900

    총알을 만들고 싶었으나 공이 됨.

---
+++ UnityBasic/UnityGP18/Assets/Scripts/Gun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject objBullet;
+    public float ShotPower;
+
+    void Shot()
+    {
+        Rigidbody2D rigidbody = objBullet.GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(Vector3.right * ShotPower);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+            Shot();
+    }
+}
