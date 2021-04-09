ef6032e0c7d54fd5961556c0f087359255dd978c
downkhg <downkhg@gmail.com>
2021-04-09 21:47:55 +0900

    부활하기와 코루틴

---
+++ UnityBasic/UnityGP18/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+        //Instantiate(collision.gameObject);//삭제된 대상을 복제하면안된다.
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+        //Instantiate(collision.gameObject);//삭제된 대상을 복제하면안된다.
+    }
+}
---
+++ UnityBasic/UnityGP18/Assets/Scripts/Responner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Responner : MonoBehaviour
+{
+    public GameObject m_prefabPlayer;
+    public GameObject m_objPlayer;
+    public bool isRespon = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_objPlayer == null && isRespon == false)
+        {
+            //게임오브젝트가 없으므로 복제에 실패한다.
+            //Instantiate(m_prefabPlayer);
+            //m_objPlayer = Instantiate(m_prefabPlayer);
+            //m_objPlayer.transform.position = this.transform.position;
+            StartCoroutine(ProcessResponTimmer());
+        }
+    }
+
+    IEnumerator ProcessResponTimmer()
+    {
+        Debug.Log("ProcessResponTimmer start");
+        isRespon = true;
+        yield return new WaitForSeconds(1);
+        ResponPlayer();
+        isRespon = false;
+        Debug.Log("ProcessResponTimmer end");
+    }
+
+    void ResponPlayer()
+    {
+        m_objPlayer = Instantiate(m_prefabPlayer);
+        m_objPlayer.transform.position = this.transform.position;
+    }
+}
