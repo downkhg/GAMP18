2c37b67876b6cf9ce4f0f7ce9adcd46b5ca6849c
downkhg <downkhg@gmail.com>
2021-06-04 21:49:25 +0900

    배열과 포인터

--- ProgramingBasic/C/5.ArrayAndPointer/5.ArrayAndPointer/5.ArrayAndPointer.cpp
+++ ProgramingBasic/C/5.ArrayAndPointer/5.ArrayAndPointer/5.ArrayAndPointer.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #define ARRAY_SIZE  5
+
+void CopyArray(const int arr[], int copyarr[], int size);
+void CopyArrayPtr(const int* arr, int* copyarr, int size);
+
 //배열: 변수를 나열하여 데이터를 순서대로 접근 가능하도록한 것.
 void ArrayMain()
 {
@@ -45,14 +49,50 @@ void ArrayMain()
 	printf("%d = %d\n", arrScores, arrCopyScore);
 
 	//memcpy(arrCopyScore, arrScores, sizeof(arrCopyScore)); //메모리카피를 이용하여 더효률적인 복사가 가능하다.
-	for (int i = 0; i < nSize; i++)
-		arrCopyScore[i] = arrScores[i];
+	//for (int i = 0; i < nSize; i++)
+	//	arrCopyScore[i] = arrScores[i];
+	CopyArrayPtr(arrScores, arrCopyScore, nSize);
 
 	for (int i = 0; i < nSize; i++)
 		printf("[%d/%d]%d,", &arrCopyScore[i], i, arrCopyScore[i]);
 }
+//const배열/포인터: 전달받은 값의 원본대상의 값을 변경할수없다.
+void CopyArray(const int arr[], int copyarr[], int size)
+{
+	printf("CopyArray[%d/%d]\n",arr,copyarr);
+	for (int i = 0; i < size; i++)
+		copyarr[i] = arr[i];
+		//arr[i] = copyarr[i]
+		
+}
+
+void CopyArrayPtr(const int* arr, int* copyarr, int size)
+{
+	printf("CopyArray[%d/%d]\n", arr, copyarr);
+	for (int i = 0; i < size; i++)
+		copyarr[i] = arr[i];
+	//arr[i] = copyarr[i]
+
+}
+
+void PointerMain()
+{
+
+	int nData = 10;
+	float fData = 3.14f;
+	int* pIntData = &nData;
+	float* pFloatData = &fData;
+	//&: 주소값을 가져옴.
+	//*: 참조하는 변수가 가진값에 접근함.
+	printf("nData[%d]:%d\n", &nData, nData);//0x01,10
+	printf("pIntData[%d]:%d\n", *pIntData, pIntData);//10, 0x01
+
+	printf("fData[%d]:%f\n", &fData, fData);//0x05,3.14
+	printf("FloatData[%f]:%d\n", *pFloatData, pFloatData);//3.14,0x05
+}
 
 void main()
 {
-	ArrayMain();
+	//ArrayMain();
+	PointerMain();
 }
