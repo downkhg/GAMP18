96038f00db039fcff49f83368c1529ce7bdd8395
downkhg <downkhg@gmail.com>
2021-07-02 21:02:49 +0900

    동적배열 크기조절 추가

--- 4.STL/4.STL/4.STL.cpp
+++ 4.STL/4.STL/4.STL.cpp
@@ -29,6 +29,7 @@ void VectorMain()
 {
 	int nSize = 10;
 	vector<int> datastruct(nSize);
+	datastruct.resize(5);//동적배열은 만든 크기를 중간에 늘리거나 줄일수있다.
 	//인덱스를 이용한 접근
 	for (int i = 0; i < datastruct.size(); i++)
 		datastruct[i] = 10 * nSize - nSize * i;
@@ -48,7 +49,7 @@ void VectorMain()
 		cout << *it << ",";
 	cout << endl;
 	//찾기
-	vector<int>::iterator itFind = find(datastruct.begin(), datastruct.end(), 50);
+	vector<int>::iterator itFind = find(datastruct.begin(), datastruct.end(), 30);
 	if (itFind != datastruct.end())
 		cout << "Find:" << *itFind << endl;
 	//삽입
@@ -78,4 +79,8 @@ void VectorMain()
 		cout << *it << ",";
 	cout << endl;	
 }
-//1.
+//1.벡터(STL)와 배열의 차이점은?
+//2.이터레이터는 포인터와 유사하다 차이점은 무엇인가?
+//3.벡터(STL)에서 만약 원하는 데이터를 찾지못한다면 어떤 값이 반환되는가?
+//4.벡터(STL)에서 삽입(insert)시 어디에 추가되는가? 그 이유는 무엇인가?
+//2.벡터(STL)에 begin()은 연결리스트의 pBegin과 같지만 end()와 pEnd는 다르다 무엇이 다른가?
