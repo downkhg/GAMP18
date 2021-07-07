f9c0b8055b1fe8f9bf98a9aca815ffb91e17cc16
downkhg <downkhg@gmail.com>
2021-07-07 21:46:47 +0900

    탬플릿 적용(템플릿은 헤더/CPP분리 불가능)

--- ProgramingBasic/DataStruct/1.LinkedList/1.LinkedList/1.1.LinkedList.h
+++ ProgramingBasic/DataStruct/1.LinkedList/1.LinkedList/1.1.LinkedList.h
@@ -1,13 +1,12 @@
 #pragma once
-#define NULL 0
+#include <stdio.h>
 
-
-
+template <typename Type> 
 class CLinkecList
 {
 public:
 	struct SNode {
-		int nData;
+		Type nData;
 		SNode* pNext;
 	};
 
@@ -20,12 +19,109 @@ public:
 		return m_pEnd;
 	}
 
-	SNode* CreateNode(int data); //노드를 생성하여 리턴한다.
-	SNode* FindNodeData(int data); //해당 데이터를 가진 노드를 찾는다.
-	SNode* InsertNodeData(int data, int insert); //해당 데이터를 가진 노드 뒤에 노드를 추가한다.
-	void DeleteNodeData(int del); //해당데이터를 가진 노드를 삭제한다.
-	void PrintLinkedList(); //노드를 순회하며 끝날때까지 출력한다.
-	void DeleteLinkedList(); //노드를 순회하며 모든데이터를 삭제한다.
+	SNode* CreateNode(Type data) //노드를 생성하여 리턴한다.
+	{
+		SNode* pTemp = NULL;
+
+		pTemp = new SNode();
+		pTemp->nData = data;
+		pTemp->pNext = NULL;
+
+		if (m_pEnd != NULL)//0x04 !=  NULL -> T
+			m_pEnd->pNext = pTemp;
+		else
+			m_pBegin = pTemp;
+
+		m_pEnd = pTemp;
+
+		return  pTemp;
+	}
+	SNode* FindNodeData(Type data) //해당 데이터를 가진 노드를 찾는다.
+	{
+		SNode* pNode = m_pBegin;
+		//while (true)
+		while (pNode)
+		{
+			if (pNode->nData != data)
+				pNode = pNode->pNext;
+			else
+				break;
+		}
+		return pNode;
+	}
+	SNode* InsertNodeData(Type data, Type insert)
+	{
+		SNode* pNode = m_pBegin;
+		SNode* pInsert = NULL;
+
+		pNode = FindNodeData(data);
+
+		if (pNode)
+		{
+			pInsert = new SNode();
+			pInsert->pNext = pNode->pNext;
+			pNode->pNext = pInsert;
+			pInsert->nData = insert;
+		}
+
+		return pInsert;
+	}
+
+ //해당 데이터를 가진 노드 뒤에 노드를 추가한다.
+	void DeleteNodeData(Type del) //해당데이터를 가진 노드를 삭제한다.
+	{
+		SNode* pPre = NULL;
+		SNode* pNode = m_pBegin;
+
+		//pNode = FindNodeData(m_pBegin, del);
+		//pPre = FindNodeData(m_pBegin, 30);
+
+		while (pNode)//0x01!=N:T
+		{
+			if (pNode->nData != del)//10!=10:F
+			{
+				pPre = pNode;
+				pNode = pNode->pNext;
+			}
+			else
+				break;
+		}
+
+		if (pNode)
+		{
+			if (pPre)
+				pPre->pNext = pNode->pNext;
+			else
+				m_pBegin = pNode->pNext;
+			delete pNode;
+		}
+	}
+	void PrintLinkedList() //노드를 순회하며 끝날때까지 출력한다.
+	{
+		SNode* pNode = m_pBegin;
+		printf("data:");
+		while (pNode)
+		{
+			printf("%d", pNode->nData);
+			pNode = pNode->pNext;
+
+			if (pNode != NULL)
+				printf(",");
+		}
+		printf("\n");
+	}
+	void DeleteLinkedList()//노드를 순회하며 모든데이터를 삭제한다.
+	{
+		SNode* pNode = m_pBegin;
+		SNode* pDel = NULL;
+		while (pNode)//0x01:T
+		{
+			pDel = pNode;
+			pNode = pNode->pNext;
+			printf("Delete:%d\n", pDel->nData);
+			delete pDel;
+		}
+	}
 	void ReverseLinkedList(); //
 private:
 	SNode* m_pBegin = NULL;
--- ProgramingBasic/DataStruct/1.LinkedList/1.LinkedList/1.1.LinkedListClass.cpp
+++ ProgramingBasic/DataStruct/1.LinkedList/1.LinkedList/1.1.LinkedListClass.cpp
@@ -1,116 +1,11 @@
 #include "1.1.LinkedList.h"
 #include <stdio.h>
 
-CLinkecList::SNode* CLinkecList::CreateNode(int data)
-{
-	SNode* pTemp = NULL;
 
-	pTemp = new SNode();
-	pTemp->nData = data;
-	pTemp->pNext = NULL;
-
-	if (m_pEnd != NULL)//0x04 !=  NULL -> T
-		m_pEnd->pNext = pTemp;
-	else
-		m_pBegin = pTemp;
-
-	m_pEnd = pTemp;
-
-	return  pTemp;
-}
-
-CLinkecList::SNode* CLinkecList::FindNodeData(int data)
-{
-	SNode* pNode = m_pBegin;
-	//while (true)
-	while (pNode)
-	{
-		if (pNode->nData != data)
-			pNode = pNode->pNext;
-		else
-			break;
-	}
-	return pNode;
-}
-
-CLinkecList::SNode* CLinkecList::InsertNodeData( int data, int insert)
-{
-	SNode* pNode = m_pBegin;
-	SNode* pInsert = NULL;
-
-	pNode = FindNodeData(data);
-
-	if (pNode)
-	{
-		pInsert = new SNode();
-		pInsert->pNext = pNode->pNext;
-		pNode->pNext = pInsert;
-		pInsert->nData = insert;
-	}
-
-	return pInsert;
-}
-
-void  CLinkecList::DeleteNodeData(int del)
-{
-	SNode* pPre = NULL;
-	SNode* pNode = m_pBegin;
-
-	//pNode = FindNodeData(m_pBegin, del);
-	//pPre = FindNodeData(m_pBegin, 30);
-
-	while (pNode)//0x01!=N:T
-	{
-		if (pNode->nData != del)//10!=10:F
-		{
-			pPre = pNode;
-			pNode = pNode->pNext;
-		}
-		else
-			break;
-	}
-
-	if (pNode)
-	{
-		if (pPre)
-			pPre->pNext = pNode->pNext;
-		else
-			m_pBegin = pNode->pNext;
-		delete pNode;
-	}
-}
-
-void  CLinkecList::PrintLinkedList()
-{
-	SNode* pNode = m_pBegin;
-	printf("data:");
-	while (pNode)
-	{
-		printf("%d", pNode->nData);
-		pNode = pNode->pNext;
-
-		if (pNode != NULL)
-			printf(",");
-	}
-	printf("\n");
-}
-
-void  CLinkecList::DeleteLinkedList()
-{
-	SNode* pNode = m_pBegin;
-	SNode* pDel = NULL;
-	while (pNode)//0x01:T
-	{
-		pDel = pNode;
-		pNode = pNode->pNext;
-		printf("Delete:%d\n", pDel->nData);
-		delete pDel;
-	}
-}
 
 void LinkedListSampleTestMain()
 {
-	CLinkecList cLinkedList;
+	CLinkecList<int> cLinkedList;
 
 	//노드 추가 테스트
 	cLinkedList.CreateNode(10);
@@ -121,11 +16,11 @@ void LinkedListSampleTestMain()
 
 	cLinkedList.PrintLinkedList();
 
-	CLinkecList::SNode* pFind = cLinkedList.FindNodeData(60);
+	CLinkecList<int>::SNode* pFind = cLinkedList.FindNodeData(60);
 	if (pFind != NULL)
 		printf("Find:%d\n", pFind->nData);
 
-	CLinkecList::SNode* pInsert = cLinkedList.InsertNodeData(30, 60);//노드 삽입
+	CLinkecList<int>::SNode* pInsert = cLinkedList.InsertNodeData(30, 60);//노드 삽입
 	if (pInsert != NULL)
 		printf("Insert:%d\n", pInsert->nData);
 
