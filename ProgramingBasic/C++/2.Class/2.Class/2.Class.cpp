3f79df3356e66f6e5a7a80fa72fc8e2e6765a158
downkhg <downkhg@gmail.com>
2021-07-19 21:03:03 +0900

    클래스의 활용

--- ProgramingBasic/C++/2.Class/2.Class/2.Class.cpp
+++ ProgramingBasic/C++/2.Class/2.Class/2.Class.cpp
@@ -11,13 +11,26 @@ class CCar
 	string m_strColor = "gray";
 	int m_nGear = N;
 public:
-	//생성자: 클래스가 객체가 생성될때 호출되는 함수.
-	CCar(string color)
+	//생성자: 클래스가 객체가 생성될때 호출되는 (함수).
+	//객체(인스턴스): 클래스의 실체가되는 메모리
+	//디폴트매개변수: 매개변수의 값을 지정하지않으면, 기본으로 들어가는 변수값.
+	CCar(string color = "gray")
 	{
 		cout << "Car[" << this << "]:" << color << endl;
 		m_strColor = color;
 	}
-	//소멸자: 클래스의 객체가 소멸될때 호출되는 함수.
+	////함수의오버로딩
+	//CCar(string color, bool engineon, int speed, int gear)//다음과 같이 차의정보를 설정 할 수 없다.
+	//{
+	//	cout << "Car[" << this << "]:" << color << endl;
+	//	m_strColor = color;
+	//}
+	////디폴트생성자: 매개변수없이 호출되는 생성자.
+	//CCar()
+	//{
+	//	cout << "Car[" << this <<"]" << endl;
+	//}
+	//소멸자: 클래스의 객체가 소멸될때 호출되는 함수. 소멸자없음.
 	~CCar()
 	{
 		cout << "~Car[" << this << "]:" << m_strColor << endl;
@@ -61,6 +74,7 @@ public:
 //주문된 자동차를 타고 시운전을 하고 주차한다.
 void CarMain()
 {
+	cout << "CarMain Start" << endl;
 	CCar cCar("blue");//자동차 객체를 생성할때 색상을 정해야한다.
 
 	cCar.TurnOn(); //시동을건다.
@@ -79,9 +93,30 @@ void CarMain()
 	cCar.Display();
 	cCar.TurnOff(); //시동을끈다.
 	cCar.Display();
+
+	cout << "CarMain End" << endl;
+}
+//※C#/Java의 클래스는 반드시 동적할당된다.
+//※C#에서 구조체를 활용하면 정적할당된다.
+void ClassTestMain()
+{
+	CCar cCarA;
+	CCar cCarB("red");
+	CCar arrCar[3];// = { CCar("red"),  CCar("green"), CCar("blue") };
+	for (int i = 0; i < 3; i++)
+		arrCar[i].Display();
+	cout << "Class Pointer 1" << endl;
+	CCar* pCar = NULL;//c++포인터 //c# 객체
+	cout << "Class Pointer DynmicAlloc 1" << endl;
+	pCar = new CCar("green");//c++ 동적할당 //c# 인스턴스
+	pCar->Display();
+	delete pCar;
+	cout << "Class Pointer DynmicAlloc 2" << endl;
+	cout << "Class Pointer 2" << endl;
 }
 
 void main()
 {
-	CarMain();
+	//CarMain();
+	ClassTestMain();
 }
