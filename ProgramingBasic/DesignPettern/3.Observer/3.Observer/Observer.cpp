5e6641b18a2720f71a6b140dcc084cda1cc31624
downkhg <downkhg@gmail.com>
2021-08-09 20:29:42 +0900

    공격추가하기

--- ProgramingBasic/DesignPettern/3.Observer/3.Observer/Observer.cpp
+++ ProgramingBasic/DesignPettern/3.Observer/3.Observer/Observer.cpp
@@ -21,14 +21,16 @@ public:
 		listUnitCollection.remove(pUnit);
 	}
 	void Move(int x, int y);
+	void Attack(int x, int y);
 };
 
-class Unit
+class Unit //클래스의 정의
 {
 	int x;
 	int y;
 public:
 	virtual void Move(int x, int y) = 0;
+	virtual void Attack(int x, int y) = 0;
 };
 
 class Marin : public Unit
@@ -37,6 +39,7 @@ public:
 	Marin();
 	~Marin();
 	void Move(int x, int y) override;
+	void Attack(int, int y) override;
 private:
 
 };
@@ -54,12 +57,18 @@ void Marin::Move(int x, int y)
 	cout << typeid(*this).name() << "::Move("<<x<<","<<y<<")" << endl;
 }
 
+void Marin::Attack(int x, int y)
+{
+	cout << typeid(*this).name() << "::Attack(" << x << "," << y << ")" << endl;
+}
+
 class Medic : public Unit
 {
 public:
 	Medic();
 	~Medic();
 	void Move(int x, int y) override;
+	void Attack(int, int y) override;
 private:
 
 };
@@ -72,6 +81,17 @@ Medic::~Medic()
 {
 }
 
+void Medic::Move(int x, int y)
+{
+	cout << typeid(*this).name() << "::Move(" << x << "," << y << ")" << endl;
+}
+
+void Medic::Attack(int x, int y)
+{
+	Move(x, y);
+}
+
+
 void Commonder::Move(int x, int y)
 {
 	list<Unit*>::iterator it = listUnitCollection.begin();
@@ -83,12 +103,19 @@ void Commonder::Move(int x, int y)
 	}
 }
 
-void Medic::Move(int x, int y)
+void Commonder::Attack(int x, int y)
 {
-	cout << typeid(*this).name() << "::Move(" << x << "," << y << ")" << endl;
+	list<Unit*>::iterator it = listUnitCollection.begin();
+	for (; it != listUnitCollection.end(); it++)
+	{
+		//Unit* pUnit = *it;
+		//pUnit->Move(x, y);
+		(*it)->Attack(x, y);
+	}
 }
 
-void main()
+
+void CommederMain()
 {
 	Commonder cCommonder;
 	vector<Unit*> vecUnits;
@@ -100,8 +127,21 @@ void main()
 	vecUnits.push_back(new Medic());
 	vecUnits.push_back(new Medic());
 
-	for(int i = 0; i<vecUnits.size(); i++)
+	for (int i = 0; i < vecUnits.size(); i++)
 		cCommonder.SelectUnit(vecUnits[i]);
 
 	cCommonder.Move(10, 10);
+	cCommonder.Deselect(vecUnits[3]);
+	cCommonder.Attack(10, 10);
+
+	for (int i = 0; i < vecUnits.size(); i++)
+	{
+		delete vecUnits[i];
+	}
+	vecUnits.clear();
+}
+
+void main()
+{
+	CommederMain();
 }
