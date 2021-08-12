f9d58611052cdc5a2ed1bcf3ca40b7dc703a33e9
downkhg <downkhg@gmail.com>
2021-08-12 21:50:17 +0900

    WinAPI, MFC

---
+++ WindowPrograming/MFCApplication/MFCApplication/MFCApplicationDlg.h
@@ -0,0 +1,37 @@
+﻿
+// MFCApplicationDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCApplicationDlg 대화 상자
+class CMFCApplicationDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCApplicationDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	CButton m_btnButton;
+};
