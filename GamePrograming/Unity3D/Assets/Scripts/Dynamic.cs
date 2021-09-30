9b3b1c108c52a2b1c0745aee7ed248b7d0f4ffdc
downkhg <downkhg@gmail.com>
2021-09-30 20:21:24 +0900

    유니티 이동하기

---
+++ GamePrograming/Unity3D/Assets/Scripts/Dynamic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamic : MonoBehaviour
+{
+    public float m_fSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * m_fSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * m_fSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up);
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.down);
+    }
+}
